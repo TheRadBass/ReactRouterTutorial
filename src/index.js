b7ef99df6c9ec0932c82bff489aa1fde5a80deab
Martin Bracamonte <martin.bracamonte@EDCMXASLT0620.local>
2023-04-06 12:02:00 -0700

    Added loaders and a new Form to create new contacts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Root from "./routes/Root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/Root";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
@@ -11,10 +14,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
